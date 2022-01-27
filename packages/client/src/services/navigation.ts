64e71957507662f2d99d5325b8a69fef6d4654e6
Gabriel Alves <gabrieltots@gmail.com>
2022-01-26 22:38:22 -0300

    add navigation service to client

---
+++ packages/client/src/services/navigation.ts
@@ -0,0 +1,22 @@
+import { createNavigationContainerRef } from '@react-navigation/native';
+import { StackActions } from '@react-navigation/native';
+import Router from 'next/router';
+import { Platform } from 'react-native';
+
+export const navigationRef = createNavigationContainerRef();
+
+export function navigateService(name, params) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name, params);
+  }
+}
+
+export function goBackService() {
+  if (Platform.OS === 'web') {
+    Router.back();
+  } else {
+    if (navigationRef.isReady()) {
+      navigationRef.goBack();
+    }
+  }
+}
