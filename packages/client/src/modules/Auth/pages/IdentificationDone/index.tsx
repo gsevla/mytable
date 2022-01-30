ab10e9cf4dd6c979e0c64117e774ea40003cb997
Gabriel Alves <gabrieltots@gmail.com>
2022-01-30 18:54:05 -0300

    IdentificationDone page refinements

--- packages/client/src/modules/Auth/pages/IdentificationDone/index.tsx
+++ packages/client/src/modules/Auth/pages/IdentificationDone/index.tsx
@@ -1,9 +1,11 @@
 import { useRouting } from 'expo-next-react-navigation';
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import { useContextSelector } from 'use-context-selector';
 import { AuthContext } from '../../context';
+import { Headline, Subheading, Text } from 'react-native-paper';
+import { SizedBox } from '../../../../components/SizedBox';
 
 export function IdentificationDonePage() {
   const handleSetActiveStep = useContextSelector(
@@ -17,35 +19,54 @@ export function IdentificationDonePage() {
   const router = useRouting();
 
   return (
-    <View
-      style={{
-        flex: 1,
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
         backgroundColor: '#eeeeee',
         paddingHorizontal: 24,
         justifyContent: 'space-between',
       }}
     >
-      <Text>IdentificationDone page</Text>
-      <Button
-        mode="text"
-        onPress={() => {
-          router.navigate({
-            routeName: 'auth/forgot-password',
-          });
-        }}
-      >
-        Não possuo mais acesso
-      </Button>
-      <Button
-        mode="contained"
-        onPress={() => {
-          router.navigate({
-            routeName: 'auth/code',
-          });
-        }}
-      >
-        Avançar
-      </Button>
-    </View>
+      <Headline style={{ textAlign: 'center' }}>
+        Olá,{'\n'}
+        <Headline>Fulano de Tal!</Headline>
+      </Headline>
+      <SizedBox h={16} />
+      <Headline style={{ textAlign: 'center' }}>Verificamos que</Headline>
+      <SizedBox h={16} />
+      <Subheading style={{ textAlign: 'center' }}>
+        Seu telefone é:{'\n'}
+        <Text>93812039180</Text>
+      </Subheading>
+      <SizedBox h={8} />
+      <Subheading style={{ textAlign: 'center' }}>
+        Seu email é:{'\n'}
+        <Text>gasgdassags</Text>
+      </Subheading>
+      <View>
+        <SizedBox h={32} />
+        <Button
+          mode="text"
+          onPress={() => {
+            router.navigate({
+              routeName: 'auth/forgot-password',
+            });
+          }}
+        >
+          Não possuo mais acesso
+        </Button>
+        <SizedBox h={24} />
+        <Button
+          mode="contained"
+          onPress={() => {
+            router.navigate({
+              routeName: 'auth/code',
+            });
+          }}
+        >
+          Avançar
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
