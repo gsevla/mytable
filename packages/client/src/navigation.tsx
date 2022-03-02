f7416d5ad25dd6b4e1bffcede4927dec6e9f92af
Gabriel Alves <gabrieltots@gmail.com>
2022-03-02 17:46:40 -0300

    root navigation adjustments

--- packages/client/src/navigation.tsx
+++ packages/client/src/navigation.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthStack from './modules/Auth/navigation';
-import { AuthorizationPage } from './modules/Auth/pages/Authorization';
+import LoadingScreen from './pages/Loading';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { AuthModule } from './modules/Auth';
 
 const RootStackNavigator = createNativeStackNavigator();
 
-export default function RootStack() {
+function App() {
+  return (
+    <View>
+      <Text>this is mytable app</Text>
+    </View>
+  );
+}
+
+interface IRootStackProps {
+  loading: boolean;
+  token: string;
+}
+
+export default function RootStack({ loading, token }: IRootStackProps) {
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <RootStackNavigator.Navigator>
-      <RootStackNavigator.Screen
-        options={{ headerShown: false }}
-        name="authStack"
-        component={AuthStack}
-      />
-      <RootStackNavigator.Screen
-        name="authorization"
-        component={AuthorizationPage}
-      />
+      {token ? (
+        <RootStackNavigator.Screen name="app" component={App} />
+      ) : (
+        <RootStackNavigator.Screen
+          options={{ headerShown: false }}
+          name="auth"
+          component={AuthModule}
+        />
+      )}
     </RootStackNavigator.Navigator>
   );
 }
