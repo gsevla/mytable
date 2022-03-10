8f67f3b00ef43cd7aced2a3375a364fe561514d2
Gabriel Alves <gabrieltots@gmail.com>
2022-03-09 23:15:00 -0300

    [client] clean up LoadingScreen

--- packages/client/src/pages/Loading/index.tsx
+++ packages/client/src/pages/Loading/index.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import { View, StatusBar } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { ActivityIndicator, Title } from 'react-native-paper';
 
 function LoadingScreen() {
-  useEffect(() => {
-    StatusBar.setHidden(true);
-
-    return () => {
-      StatusBar.setHidden(false);
-    };
-  }, []);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#eee',
+      }}
+    >
       <ActivityIndicator size="large" />
       <Title>Carregando</Title>
     </View>
