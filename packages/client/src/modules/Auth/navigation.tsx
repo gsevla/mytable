7f9fd3e3cdaf6c39792612ac0da16c7f94cc76ba
Gabriel Alves <gabrieltots@gmail.com>
2022-03-11 18:27:11 -0300

    auth navigation groups and render based on client

--- packages/client/src/modules/Auth/navigation.tsx
+++ packages/client/src/modules/Auth/navigation.tsx
@@ -5,30 +5,43 @@ import { AuthorizationCodePage } from './pages/AuthorizationCode';
 import { IdentificationPage } from './pages/Identification';
 import { IdentificationDonePage } from './pages/IdentificationDone';
 import { ForgotPasswordPage } from './pages/ForgotPassword';
-import { AuthorizationPage } from './pages/Authorization';
+import { useContextSelector } from 'use-context-selector';
+import { RootContext } from '../Root/context';
 
 const AuthStackNavigator = createNativeStackNavigator();
 
 export default function AuthStack() {
+  const client = useContextSelector(RootContext, (values) => values.client);
+
   return (
     <AuthStackNavigator.Navigator screenOptions={{ headerShown: false }}>
-      <AuthStackNavigator.Screen name="ask-for-cpf" component={AskForCpfPage} />
-      <AuthStackNavigator.Screen
-        name="identification"
-        component={IdentificationPage}
-      />
-      <AuthStackNavigator.Screen
-        name="forgot-password"
-        component={ForgotPasswordPage}
-      />
-      <AuthStackNavigator.Screen
-        name="identification-done"
-        component={IdentificationDonePage}
-      />
-      <AuthStackNavigator.Screen
-        name="authorization"
-        component={AuthorizationCodePage}
-      />
+      {!client ? (
+        <AuthStackNavigator.Group>
+          <AuthStackNavigator.Screen
+            name="ask-for-cpf"
+            component={AskForCpfPage}
+          />
+          <AuthStackNavigator.Screen
+            name="identification"
+            component={IdentificationPage}
+          />
+        </AuthStackNavigator.Group>
+      ) : (
+        <AuthStackNavigator.Group>
+          <AuthStackNavigator.Screen
+            name="identification-done"
+            component={IdentificationDonePage}
+          />
+          <AuthStackNavigator.Screen
+            name="forgot-password"
+            component={ForgotPasswordPage}
+          />
+          <AuthStackNavigator.Screen
+            name="authorization"
+            component={AuthorizationCodePage}
+          />
+        </AuthStackNavigator.Group>
+      )}
     </AuthStackNavigator.Navigator>
   );
 }
