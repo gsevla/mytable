f9cdc5bc8f5e2397235f18c1a43340d4aac26169
Gabriel Alves <gabrieltots@gmail.com>
2022-03-11 18:22:06 -0300

    [client] supress setTimeout warning on expo app root

--- packages/client/App.tsx
+++ packages/client/App.tsx
@@ -1,66 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useRef, useState } from 'react';
-import { AppState, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { LogBox } from 'react-native';
 import { AppRoot } from './src';
-import * as Linking from 'expo-linking';
-import { navigateService } from './src/services/navigation';
+
 import './src/services/api';
 
 export default function App() {
-  // const appState = useRef(AppState.currentState);
-  // const [appStateVisible, setAppStateVisible] = useState(appState.current);
-
-  // useEffect(() => {
-  //   AppState.addEventListener('change', _handleAppStateChange);
-
-  //   return () => {
-  //     AppState.removeEventListener('change', _handleAppStateChange);
-  //   };
-  // }, []);
-
-  // const _handleAppStateChange = (nextAppState) => {
-  //   if (
-  //     appState.current.match(/inactive|background/) &&
-  //     nextAppState === 'active'
-  //   ) {
-  //     console.log('App has come to the foreground!');
-  //   }
-
-  //   appState.current = nextAppState;
-  //   setAppStateVisible(appState.current);
-  //   console.log('AppState', appState.current);
-  // };
-
-  // function handleDeepLink(event: Linking.EventType) {
-  //   let data = Linking.parse(event.url);
-  //   // Linking.openURL('exp://127.0.0.1:19000/--/authorization');
-  //   console.log('data', data);
-  //   navigateService(data.path, data.queryParams);
-  // }
-
-  // useEffect(() => {
-  //   Linking.getInitialURL().then(
-  //     (value) => {
-  //       console.log('initialUrl', value);
-  //       if (value) {
-  //         let data = Linking.parse(value);
-  //         console.log('data', data);
-  //         navigateService(data.path, data.queryParams);
-  //       }
-  //     },
-  //     (error) => {
-  //       console.log('err', error);
-  //     },
-  //   );
-  // }, []);
-
-  // useEffect(() => {
-  //   Linking.addEventListener('url', handleDeepLink);
-
-  //   return () => {
-  //     Linking.removeEventListener('url');
-  //   };
-  // }, []);
+  useEffect(() => {
+    LogBox.ignoreLogs(['Setting a timer']);
+  }, []);
 
   return <AppRoot />;
 }
