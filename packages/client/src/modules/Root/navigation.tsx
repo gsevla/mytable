297a4402c8d377fe040dd20f6fec58f71780ed61
Gabriel Alves <gabrieltots@gmail.com>
2022-03-11 17:11:27 -0300

    [client] move Root navigation to Root Module

--- packages/client/src/modules/Root/navigation.tsx
+++ packages/client/src/modules/Root/navigation.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoadingScreen from './pages/Loading';
+import LoadingScreen from '../../pages/Loading';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
-import { AuthModule } from './modules/Auth';
+import { AuthModule } from '../Auth';
 import { useContextSelector } from 'use-context-selector';
-import { RootContext } from './modules/Root/context';
+import { RootContext } from './context';
 
 const RootStackNavigator = createNativeStackNavigator();
 
@@ -17,23 +17,19 @@ function App() {
   );
 }
 
-interface IRootStackProps {
-  loading: boolean;
-  token: string;
-}
-
 export default function RootStack() {
+  const loading = useContextSelector(RootContext, (values) => values.loading);
   const loaded = useContextSelector(RootContext, (values) => values.loaded);
+  const client = useContextSelector(RootContext, (values) => values.client);
+  const token = useContextSelector(RootContext, (values) => values.token);
 
-  const token = '';
-
-  if (!loaded) {
+  if (loading || !loaded) {
     return <LoadingScreen />;
   }
 
   return (
     <RootStackNavigator.Navigator>
-      {token ? (
+      {client && token ? (
         <RootStackNavigator.Screen name="app" component={App} />
       ) : (
         <RootStackNavigator.Screen
