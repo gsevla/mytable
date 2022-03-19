9cee94c73fb350abd44cf669bdf7ce9756f0827e
Gabriel Alves <gabrieltots@gmail.com>
2022-03-19 14:50:47 -0300

    [client] adjust next _app with providers

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,41 +1,16 @@
 import { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
 import { AuthContextProvider } from '../src/modules/Auth/context';
-import { ThemeProvider } from '../src/modules/Theme';
-import { ApiService, StorageService } from '../src/services';
+import { RootContextProvider } from '../src/modules/Root/context';
+import { ApiService } from '../src/services';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [restaurant, setRestaurant] = useState();
-
-  useEffect(() => {
-    ApiService.resources.restaurant
-      .getRestaurant()
-      .then((response) => {
-        if (response.data) {
-          setRestaurant(response.data);
-          StorageService.setData({
-            key: StorageService.keys.restaurant,
-            value: JSON.stringify(response.data),
-          }).catch((error) => {
-            console.log('error on get restaurant from storage');
-          });
-        }
-      })
-      .catch((error) => {
-        console.log('error on getRestaurant', error);
-      })
-      .finally(() => {
-        // setLoading(false);
-      });
-  }, []);
   return (
-    <ThemeProvider
-      primaryColor={restaurant?.primaryColor}
-      accentColor={restaurant?.accentColor}
-    >
-      <AuthContextProvider>
-        <Component {...pageProps} />
-      </AuthContextProvider>
-    </ThemeProvider>
+    <ApiService.QueryClientProvider>
+      <RootContextProvider>
+        <AuthContextProvider>
+          <Component {...pageProps} />
+        </AuthContextProvider>
+      </RootContextProvider>
+    </ApiService.QueryClientProvider>
   );
 }
