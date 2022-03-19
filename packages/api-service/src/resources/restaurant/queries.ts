f0ae881f1ce3f5bf67acd7d989061aa146b9d227
Gabriel Alves <gabrieltots@gmail.com>
2022-03-19 14:50:00 -0300

    [api-service] update restaurant query

--- packages/api-service/src/resources/restaurant/queries.ts
+++ packages/api-service/src/resources/restaurant/queries.ts
@@ -1,18 +1,28 @@
 import { RestaurantDto } from '@mytable/dtos';
 import { AxiosError, AxiosResponse } from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, UseQueryOptions } from 'react-query';
 import { createRestaurantEndpoints } from './http';
 import { restaurantQueryKeys } from './keys';
 
 export function createRestaurantQueries(
   restaurantEndpoints: ReturnType<typeof createRestaurantEndpoints>,
 ) {
-  function useQueryRestaurant() {
+  function useQueryRestaurant(
+    options?: UseQueryOptions<
+      RestaurantDto.IRestaurant,
+      AxiosError<{
+        error: string;
+        message: string;
+        statusCode: number;
+      }>
+    >,
+  ) {
     const _queryKey = [restaurantQueryKeys.restaurant];
 
     return useQuery<RestaurantDto.IRestaurant, AxiosError>(
       _queryKey,
       restaurantEndpoints.getRestaurant,
+      options,
     );
   }
 
