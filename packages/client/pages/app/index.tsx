bf34ded2abe48669ffcfbc84303bcc5def87f226
Gabriel Alves <gabrieltots@gmail.com>
2022-03-19 23:15:02 -0300

    [client] add app web page (post authentication)

---
+++ packages/client/pages/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View>
+      <Text>this is the app!</Text>
+    </View>
+  );
+}
