9fcd4d387432c2f240f9bfe96116602a3875c7f0
Gabriel Alves <gabrieltots@gmail.com>
2022-03-27 11:39:28 -0300

    ts configured

--- config/eslint-config/.eslintrc.js
+++ config/eslint-config/.eslintrc.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   env: {
     browser: true,
@@ -10,8 +12,8 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.eslint.json',
-    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    tsconfigRootDir: path.join(__dirname, '..', '..'),
     ecmaFeatures: {
       jsx: true,
     },
@@ -24,9 +26,45 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'airbnb-typescript',
     'prettier',
   ],
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  plugins: ['react', '@typescript-eslint', 'import'],
+  rules: {
+    "import/no-unresolved": "error"
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        "project": path.join(__dirname, '..', '..'),
+
+        // Multiple tsconfigs (Useful for monorepos)
+
+        // use a glob pattern
+        // "project": "packages/*/tsconfig.json",
+
+        // use an array
+        // "project": [
+        //   "packages/module-a/tsconfig.json",
+        //   "packages/module-b/tsconfig.json"
+        // ],
+
+        // use an array of glob patterns
+        "project": [
+          path.join(__dirname, '..', '..', 'apps', '*', 'tsconfig.json'),
+          path.join(__dirname, '..', '..', 'packages', '*', 'tsconfig.json'),
+        ]
+      }
+    }
+  }
 };
