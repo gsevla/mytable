e1da62b99865de2a21ca401a5d090a9137695486
Gabriel Alves <gabrieltots@gmail.com>
2022-04-21 19:59:28 -0300

    [client] add App module navigation(tab navigator)

---
+++ apps/client/src/modules/App/navigation/index.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from '../../../components/Icon';
+
+const AppBottomTabNavigator = createBottomTabNavigator();
+
+function Restaurant() {
+  return (
+    <View>
+      <Text>this is restaurant</Text>
+    </View>
+  );
+}
+
+function Reservation() {
+  return (
+    <View>
+      <Text>this is reservation</Text>
+    </View>
+  );
+}
+
+function WaitingLine() {
+  return (
+    <View>
+      <Text>this is waiting line</Text>
+    </View>
+  );
+}
+
+function Identification() {
+  return (
+    <View>
+      <Text>this is identification</Text>
+    </View>
+  );
+}
+
+function SignOut() {
+  return null;
+}
+
+export function AppBottomTab() {
+  return (
+    <AppBottomTabNavigator.Navigator
+      defaultScreenOptions={{
+        headerTitle: '',
+      }}
+    >
+      <AppBottomTabNavigator.Screen
+        name='Restaurant'
+        options={{
+          tabBarLabel: 'Restaurante',
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name='home-outline'
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={Restaurant}
+      />
+      <AppBottomTabNavigator.Screen
+        name='Reservation'
+        options={{
+          tabBarLabel: 'Reservas',
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name='clock-outline'
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        component={Reservation}
+      />
+      <AppBottomTabNavigator.Screen
+        name='WaitingLine'
+        component={WaitingLine}
+        options={{
+          tabBarLabel: 'Fila de espera',
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name='human-male-female'
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <AppBottomTabNavigator.Screen
+        name='Identification'
+        component={Identification}
+        options={{
+          tabBarLabel: 'Identificação',
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name='fingerprint'
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <AppBottomTabNavigator.Screen
+        name='SignOut'
+        component={SignOut}
+        options={{
+          tabBarLabel: 'Sair',
+          tabBarIcon: ({ color, size }) => (
+            <Icon
+              name='logout'
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </AppBottomTabNavigator.Navigator>
+  );
+}
