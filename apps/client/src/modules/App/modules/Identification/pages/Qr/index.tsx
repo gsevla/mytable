3ffa8713d9ccdf9cb6e3072c62be1c9a5609b1f0
Gabriel Alves <gabrieltots@gmail.com>
2022-04-21 21:26:53 -0300

    [client] AppIdentificationQrPage layout done

--- apps/client/src/modules/App/modules/Identification/pages/Qr/index.tsx
+++ apps/client/src/modules/App/modules/Identification/pages/Qr/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Headline, Caption } from 'react-native-paper';
+import { SizedBox } from '../../../../../../components/SizedBox';
 
 export function AppIdentificatioQrPage() {
   return (
-    <View>
-      <Text>AppIdentificatioQrPage</Text>
+    <View style={styles.container}>
+      <Headline style={styles.headline}>
+        Esse é o seu código de {'\n'}identificação.
+      </Headline>
+      <SizedBox />
+      <Headline style={styles.headline}>
+        Exiba-o na recepção caso {'\n'}queira entrar na fila de {'\n'}espera.
+      </Headline>
+
+      <SizedBox />
+
+      <View style={styles.imageSimulation} />
+
+      <Caption>Código</Caption>
+      <Caption style={styles.captionBold}>XXXXXXXXX</Caption>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  headline: {
+    textAlign: 'center',
+  },
+  imageSimulation: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#aaa',
+  },
+  captionBold: {
+    fontWeight: 'bold',
+  },
+});
