a73cabb6f9a018184310bf59d4d7d4f7a210608b
Gabriel Alves <gabrieltots@gmail.com>
2022-04-22 14:44:28 -0300

    [client] AppIdentificationYourTurnPage layout

--- apps/client/src/modules/App/modules/Identification/pages/YourTurn/index.tsx
+++ apps/client/src/modules/App/modules/Identification/pages/YourTurn/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Button, Headline } from 'react-native-paper';
 
 export function AppIdentificationYourTurnPage() {
   return (
-    <View>
-      <Text>AppIdentificationYourTurnPage</Text>
+    <View style={styles.container}>
+      <Headline style={styles.headlingCentered}>
+        Sua vez chegou, {'\n'}dirija-se à recepção!
+      </Headline>
+
+      {/* TODO */}
+      {/* Componente de congratulações */}
+      <View style={styles.imageSimulation} />
+
+      <Button
+        mode='contained'
+        onPress={() => {}}
+      >
+        Confirmar
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 24,
+  },
+  imageSimulation: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#aaa',
+  },
+  headlingCentered: {
+    textAlign: 'center',
+  },
+});
