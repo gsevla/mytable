2986560983bfa3413a4c32d495c6f2924736ef02
Gabriel Alves <gabrieltots@gmail.com>
2022-04-22 14:50:44 -0300

    [client] AppIdentificationLooseYourTurnPage layout

--- apps/client/src/modules/App/modules/Identification/pages/LooseYourTurn/index.tsx
+++ apps/client/src/modules/App/modules/Identification/pages/LooseYourTurn/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Button, Headline } from 'react-native-paper';
 
 export function AppIdentificationLooseYourTurnPage() {
   return (
-    <View>
-      <Text>AppIdentificationLooseYourTurnPage</Text>
+    <View style={styles.container}>
+      <Headline>Ops! Parece que {'\n'}você perdeu a vez.</Headline>
+
+      {/* TODO */}
+      {/* Componente de sinto muito */}
+      <View style={styles.imageSimulation} />
+
+      <View>
+        <Headline>Sentimos muito!</Headline>
+        <Headline>
+          Você pode entrar {'\n'}novamente na {'\n'}fila caso deseje.
+        </Headline>
+      </View>
+
+      <Button
+        mode='contained'
+        onPress={() => {}}
+      >
+        Confirmar
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 24,
+  },
+  imageSimulation: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#aaa',
+  },
+});
