1144a2dc838c820a00ad09d2d77808b87f42a92e
Gabriel Alves <gabrieltots@gmail.com>
2022-04-21 21:41:34 -0300

    [client] AppIdentificationWaitingPage layout done

--- apps/client/src/modules/App/modules/Identification/pages/Waiting/index.tsx
+++ apps/client/src/modules/App/modules/Identification/pages/Waiting/index.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Button, Headline } from 'react-native-paper';
+import { SizedBox } from '../../../../../../components/SizedBox';
 
 export function AppIdentificationWaitingPage() {
   return (
-    <View>
-      <Text>AppIdentificationWaitingPage</Text>
+    <View style={styles.container}>
+      <View>
+        <Headline>
+          Fulano, você está {'\n'}na{' '}
+          <Headline style={styles.headlineBold}>X</Headline> posição
+        </Headline>
+        <SizedBox />
+        <Headline>
+          <Headline style={styles.headlineBold}>Y</Headline> pessoas {'\n'}estão
+          na sua frente
+        </Headline>
+      </View>
+
+      {/* TODO */}
+      {/* Componente de relógio */}
+      <View style={styles.imageSimulation} />
+
+      <Button
+        mode='contained'
+        onPress={() => {}}
+      >
+        Sair da fila de espera
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 24,
+  },
+  headlineBold: {
+    fontWeight: 'bold',
+  },
+  imageSimulation: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#aaa',
+  },
+});
