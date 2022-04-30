3edab142dcc8197a9600d0ae8e3dc1bd0c27a40e
Gabriel Alves <gabrieltots@gmail.com>
2022-04-30 15:49:40 -0300

    [client] add Restaurant EnvironmentSection component

---
+++ apps/client/src/modules/App/modules/Restaurant/components/EnvironmentSection/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Subheading, Paragraph, Caption } from 'react-native-paper';
+import { SizedBox } from '../../../../../../components/SizedBox';
+import { AppRestaurantImageComponent } from '../Image';
+
+interface Props {
+  title: string;
+  description: string;
+  images?: Array<{
+    id: number;
+    source: NodeRequire;
+  }>;
+}
+
+export function AppRestaurantEnvironmentSectionComponent({
+  title,
+  description,
+  images,
+}: Props) {
+  return (
+    <View>
+      <Subheading style={styles.spaceTitle}>{title}</Subheading>
+      <Paragraph>{description}</Paragraph>
+      {(() => {
+        if (!images) {
+          return <Caption>(Imagens não disponibilizadas)</Caption>;
+        }
+
+        if (images.length > 0) {
+          <ScrollView horizontal>
+            {images.map((item, index, arr) => {
+              return (
+                <React.Fragment key={item.id.toString()}>
+                  <AppRestaurantImageComponent
+                    imageProps={{ source: item.source }}
+                  />
+                  {index < arr.length - 1 && <SizedBox />}
+                </React.Fragment>
+              );
+            })}
+          </ScrollView>;
+        }
+
+        return null;
+      })()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  spaceTitle: {
+    fontWeight: 'bold',
+  },
+});
