ecc162acfd327e3fd8712cbcc350f38768d61d18
Gabriel Alves <gabrieltots@gmail.com>
2022-05-28 17:23:06 -0300

    add reservationOrder dto

--- packages/dtos/index.ts
+++ packages/dtos/index.ts
@@ -1,5 +1,6 @@
 import * as ClientDto from './client';
 import * as EmployeeDto from './employee';
 import * as RestaurantDto from './restaurant';
+import * as ReservationOrderDto from './reservationOrder';
 
-export { ClientDto, EmployeeDto, RestaurantDto };
+export { ClientDto, EmployeeDto, RestaurantDto, ReservationOrderDto };
