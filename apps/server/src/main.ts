b6e5dc90eb37c72a5d9144c64207d1de129bcc30
Gabriel Alves <gabrieltots@gmail.com>
2022-07-02 18:19:13 -0300

    [server] add swagger under /docs

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as morgan from 'morgan';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api', { exclude: ['/'] });
+  app.setGlobalPrefix('api', { exclude: ['/', 'docs'] });
   app.enableCors();
   app.use(morgan('tiny'));
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('MyTable')
+    .setDescription('MyTable API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.SERVER_PORT);
 }
+
 bootstrap();
