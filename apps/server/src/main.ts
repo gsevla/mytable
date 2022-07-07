3e1407e1a061f9fa24e5bed642faa835fc04e4b1
Gabriel Alves <gabrieltots@gmail.com>
2022-07-06 22:12:42 -0300

    [server] enable docs bearerAuth

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .setTitle('MyTable')
     .setDescription('MyTable API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
