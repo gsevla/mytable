2096a8fa55437cc7fbe1fb7098841d605b6916cf
Gabriel Alves <gabrieltots@gmail.com>
2022-08-20 19:36:53 -0300

    [restaurant] start employees screen

--- apps/restaurant/pages/app/employee/index.tsx
+++ apps/restaurant/pages/app/employee/index.tsx
@@ -1,13 +1,28 @@
 import { AppPageWrapper } from 'components/AppPageWrapper';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
+import ApiService from '#/services/api';
 
 export default function AppEmployeePage() {
+  const [employees, setEmployees] = useState([]);
+
+  const getEmployees = async () => {
+    const response = await ApiService.axiosInstance.get('/employee');
+    setEmployees(response.data);
+  };
+
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
   return (
     <AppPageWrapper>
       <View>
         <Text>Employee screen</Text>
+        {employees.map((item) => (
+          <Text>{item.name}</Text>
+        ))}
       </View>
     </AppPageWrapper>
   );
