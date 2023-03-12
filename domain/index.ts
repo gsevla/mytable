2a5685d6bf5b1f74779fa702d1538859486ce751
Gabriel Alves <gabrieltots@gmail.com>
2023-03-12 18:44:26 -0300

    [domain] export index

---
+++ domain/index.ts
@@ -0,0 +1,10 @@
+// entities
+export * from './entities/Client';
+export * from './entities/Employee';
+export * from './entities/Restaurant';
+export * from './entities/EnvironmentImage';
+export * from './entities/WorkingDay';
+
+// enums
+export * from './enums/Day';
+export * from './enums/EmployeeRole';
