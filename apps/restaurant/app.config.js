7f07c0a90b036ce23b7103b215243e185e51c3a2
Gabriel Alves <gabriel.alves@superlogica.com>
2023-04-30 12:15:22 -0300

    add socket requirements

--- apps/restaurant/app.config.js
+++ apps/restaurant/app.config.js
@@ -1,12 +1,12 @@
 import { sync as findUpSync } from 'find-up';
 import dotenv from 'dotenv';
+
 dotenv.config({ path: findUpSync('.env') });
 
-export default ({ config }) => {
-  return {
-    ...config,
-    extra: {
-      API_URL: process.env.API_URL,
-    },
-  };
-};
+export default ({ config }) => ({
+  ...config,
+  extra: {
+    API_URL: process.env.API_URL,
+    SOCKET_ADDR: process.env.SOCKET_URL,
+  },
+});
