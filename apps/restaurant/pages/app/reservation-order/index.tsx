a790fc8bc9b5042fc43bd87aadbc0e7b598c73d6
gsevla <gabrieltots@gmail.com>
2023-05-03 22:31:30 -0300

    [restaurant] change reservation order top tab nav to paper bottom nav

--- apps/restaurant/pages/app/reservation-order/index.tsx
+++ apps/restaurant/pages/app/reservation-order/index.tsx
@@ -1,11 +1,28 @@
 import { AppPageWrapper } from 'components/AppPageWrapper';
 import React from 'react';
-import { AppReservationTopTab } from './components/ReservationTopTab';
+import { BottomNavigation } from 'react-native-paper';
+import { ActiveReservationTab } from './components/ActiveReservationTab';
+import { ReservationHistoryTab } from './components/ReservationHistoryTab';
 
 export default function AppReservationOrderPage() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'activeReservation', title: 'Em andamento' },
+    { key: 'reservationHistory', title: 'Histórico' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    activeReservation: ActiveReservationTab,
+    reservationHistory: ReservationHistoryTab,
+  });
+
   return (
     <AppPageWrapper isLoading={false}>
-      <AppReservationTopTab />
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
     </AppPageWrapper>
   );
 }
