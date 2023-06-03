ed008830ba621adfd4a3e723a4c526ca9247f9ed
gsevla <gabrieltots@gmail.com>
2023-06-03 02:27:38 -0300

    [client] make babel respect platform specific plugins

--- apps/client/babel.config.js
+++ apps/client/babel.config.js
@@ -1,28 +1,38 @@
 // @generated: @expo/next-adapter@3.1.0
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
 
-module.exports = {
-  presets: ['@expo/next-adapter/babel'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        extensions: ['.js', '.ts', '.tsx'],
-        alias: {
-          '~': './src/',
-          '#services': './src/services/',
-          '#providers': './providers/',
-          '#hooks': './hooks/',
+const { PLATFORM } = process.env;
+
+const isMobilePlatform = PLATFORM !== 'web';
+
+module.exports = (api) => {
+  api.cache.using(() => process.env.PLATFORM);
+
+  return {
+    presets: ['@expo/next-adapter/babel'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.js', '.ts', '.tsx'],
+          alias: {
+            '~': './src/',
+            '#services': './src/services/',
+            '#providers': './providers/',
+            '#hooks': './hooks/',
+          },
         },
-      },
-    ],
-    ['@babel/plugin-transform-flow-strip-types'],
-    process.env.PLATFORM === 'web'
-      ? ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
-      : undefined,
-    process.env.PLATFORM === 'web'
-      ? ['@babel/plugin-proposal-private-methods', { loose: true }]
-      : undefined,
-  ],
+      ],
+      ['@babel/plugin-transform-flow-strip-types'],
+      !isMobilePlatform && [
+        '@babel/plugin-proposal-private-property-in-object',
+        { loose: true },
+      ],
+      !isMobilePlatform && [
+        '@babel/plugin-proposal-private-methods',
+        { loose: true },
+      ],
+    ].filter(Boolean),
+  };
 };
