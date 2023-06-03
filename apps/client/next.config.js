87c99762c6c4ab1a2ff17525904eef674df70721
gsevla <gabrieltots@gmail.com>
2023-06-03 02:07:05 -0300

    [client] enable pwa

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,9 +1,13 @@
 // @generated: @expo/next-adapter@3.1.0
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 
+const withPlugins = require('next-compose-plugins');
 const { withExpo } = require('@expo/next-adapter');
 const withFonts = require('next-fonts');
 const withImages = require('next-images');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
 
 const withTM = require('next-transpile-modules')([
   'react-native-web',
@@ -14,15 +18,25 @@ const withTM = require('next-transpile-modules')([
   '@mytable/domain',
 ]);
 
-module.exports = withExpo(
-  withTM(
-    withFonts(
-      withImages({
+module.exports = withPlugins(
+  [
+    withTM,
+    withPWA,
+    [
+      withExpo,
+      {
         projectRoot: __dirname,
+      },
+    ],
+    [withFonts],
+    [
+      withImages,
+      {
         images: {
           disableStaticImages: true,
         },
-      })
-    )
-  )
+      },
+    ],
+  ],
+  {}
 );
--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { AuthContextProvider } from '../src/modules/Auth/context';
 import { RootContextProvider } from '../src/modules/Root/context';
 import { Providers } from '../providers';
@@ -10,6 +11,25 @@ export default function App({ Component, pageProps }: AppProps) {
     <Providers>
       <RootContextProvider>
         <AuthContextProvider>
+          <Head>
+            <title>MyTable</title>
+            <link
+              rel='shortcut icon'
+              href='/img/icon-512x512.png'
+            />
+            <link
+              rel='apple-touch-icon'
+              href='/img/icon-512x512.png'
+            />
+            <link
+              rel='manifest'
+              href='/manifest.json'
+            />
+            <meta
+              name='theme-color'
+              content='#128a8a'
+            />
+          </Head>
           <Component {...pageProps} />
         </AuthContextProvider>
       </RootContextProvider>
